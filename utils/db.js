6bda67714e22cf4ea46234398bb9186246d6bdbc
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-12-05 16:28:26 +0100

    Update utils db

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,11 @@
 import { MongoClient } from 'mongodb';
 
+require('dotenv').config();
+
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${HOST}:${PORT}`;
+const url = process.env.DB_CONNECT || `${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
